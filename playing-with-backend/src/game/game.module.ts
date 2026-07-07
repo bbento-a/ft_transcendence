ff3a1366091a4180353c4e3a75ac24cc97d09726
redgtxt <hp052635@gmail.com>
2026-07-07 13:43:24 +0100

    added jwt in to game provider

--- playing-with-backend/src/game/game.module.ts
+++ playing-with-backend/src/game/game.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
 import { GameGateway } from './game.gateway';
 
 @Module({
+  imports: [
+    JwtModule.register({
+    secret: process.env.JWT_SECRET!,
+    signOptions: { expiresIn: '1d' },
+  }),
+  ],
   providers: [GameService, GameGateway],
-  controllers: [GameController]
+  controllers: [GameController],
 })
+
 export class GameModule {}
