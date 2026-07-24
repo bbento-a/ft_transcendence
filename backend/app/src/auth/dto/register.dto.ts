7cffb86f2623dd4bd421c2df6fe6ae7eb930ce54
redgtxt <hp052635@gmail.com>
2026-07-24 17:12:17 +0100

    changed the max size of a nickname

--- backend/app/src/auth/dto/register.dto.ts
+++ backend/app/src/auth/dto/register.dto.ts
@@ -19,7 +19,7 @@ export class RegisterDto {
   @IsString()
   @IsNotEmpty({ message: 'Username cannot be empty.' })
   @MinLength(3, { message: 'Username must be at least 3 characters long.' })
-  @MaxLength(20, { message: 'Username cannot exceed 20 characters.' })
+  @MaxLength(12, { message: 'Username cannot exceed 12 characters.' })
   @Matches(/^[a-zA-Z0-9_]+$/, { message: 'Username can only contain letters, numbers and underscores.' })
   username!: string;
 
